4e5b21714dd0d29032fbe0afac8ae8ea3ec5b1be
Henry Withoot <osc2nuke@gmail.com>
2018-07-31 22:11:10 +0200

    Remove obsolete files

--- admin/includes/modules/boxes/templates/information.php
+++
@@ -1,7 +0,0 @@
-<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"><?php echo MODULE_ADMIN_BOXES_INFORMATION_BOX_TITLE; ?></h6>
-<ul class="nav flex-column mb-2">
-    <li class="nav-item"><a class="nav-link" href="<?php echo tep_href_link('shipping.php'); ?>"><?php echo MODULE_ADMIN_BOXES_INFORMATION_BOX_SHIPPING; ?></a></li>
-    <li class="nav-item"><a class="nav-link" href="<?php echo tep_href_link('privacy.php'); ?>"><?php echo MODULE_ADMIN_BOXES_INFORMATION_BOX_PRIVACY; ?></a></li>
-    <li class="nav-item"><a class="nav-link" href="<?php echo tep_href_link('conditions.php'); ?>"><?php echo MODULE_ADMIN_BOXES_INFORMATION_BOX_CONDITIONS; ?></a></li>
-    <li class="nav-item"><a class="nav-link" href="<?php echo tep_href_link('contact_us.php'); ?>"><?php echo MODULE_ADMIN_BOXES_INFORMATION_BOX_CONTACT; ?></a></li>
-</ul>
